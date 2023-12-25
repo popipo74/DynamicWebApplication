d7fa4bb82a8a044f90956d6266b6c4bfa19b784b
popipo74 <46301752+popipo74@users.noreply.github.com>
2023-12-26 03:43:08 +0800

    Update SimpleFlask.sh

--- SimpleFlask.sh
+++ SimpleFlask.sh
@@ -10,27 +10,3 @@ mkdir flask_app
 # Create simple Flask app
 echo "from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, Flask on EC2!'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')" > flask_app/app.py
-
-# Execute Flask App
-python3 app.py
-
-sudo yum install httpd
-
-echo " <VirtualHost *:80>
-    ServerName yourdomain.com
-
-    ProxyPass / http://127.0.0.1:5000/
-    ProxyPassReverse / http://127.0.0.1:5000/
-</VirtualHost> " > /etc/httpd/conf/httpd.conf
-
-sudo service httpd restart
-
-echo "Completed"
