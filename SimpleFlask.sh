36fcd87a0d5feb2c25ab75f55154c34f1398a8a3
popipo74 <46301752+popipo74@users.noreply.github.com>
2023-12-26 09:45:24 +0800

    Update SimpleFlask.sh

--- SimpleFlask.sh
+++ SimpleFlask.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Install python & flask
+sudo yum install httpd -y
 sudo yum install -y python3 python3-pip
 pip3 install flask
 
 # Create flask app directory
 mkdir flask_app
 
-# Create simple Flask app
-echo "from flask import Flask
-
-
 cd flask_app
 
-echo "in flask_app directory"
+# Create simple Flask app
+echo " from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask on EC2!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0') " > app.py 
+
+    
